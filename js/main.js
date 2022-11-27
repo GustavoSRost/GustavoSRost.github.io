16942a8f9a17fe545aebb55214ca1a6a16c44e6a
Gustavo Rost <gustavosrost@outlook.com>
2022-11-27 03:09:12 -0300

    Fim header mobile/ inicio middle mob

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-function checkScrolling() {
-  window.scrollY > 20
-    ? document.body.classList.add("scrolled")
-    : document.body.classList.remove("scrolled");
+function checkScroll() {
+  function checkScrolling() {
+    window.scrollY > 20
+      ? document.body.classList.add("scrolled")
+      : document.body.classList.remove("scrolled");
+  }
+  window.addEventListener("scroll", checkScrolling);
 }
-window.addEventListener("scroll", checkScrolling);
+
+function dropdownMenu() {
+  const button = document.querySelector(".hamburger-button");
+  const menu = document.querySelector(".header-menu");
+  function toggleMenu(e) {
+    e.preventDefault();
+    document.body.classList.toggle("hamburgerOpened");
+    button.classList.toggle("opened");
+    menu.classList.toggle("opened");
+  }
+  button.addEventListener("click", toggleMenu);
+}
+function scriptReady() {
+  dropdownMenu();
+  checkScroll();
+}
+window.addEventListener("DOMContentLoaded", scriptReady);
